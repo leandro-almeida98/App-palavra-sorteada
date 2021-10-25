780356e8a4de7214f90266188ab9b4a9889f6208
Leandro Almeida <39975179+leandro-almeida98@users.noreply.github.com>
2021-10-25 15:01:26 -0300

    Commit de Interface V1
    Commit de Interface V1

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image,
+  TouchableOpacity 
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First commit Palavra Sorteada!</Text>
-      <StatusBar style="auto" />
+    
+        <Image 
+        source={require('./src/biscoito.png')}
+        style={styles.img}
+         />
+
+        <Text style={styles.textoFrase}>"Alguma frase aqui "</Text>
+        <TouchableOpacity style={styles.botao}>
+          <View style={styles.area}>
+            <Text style={styles.texto}>Sortear</Text>
+          </View>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +34,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img:{
+      width:250,
+      height:250
+  },
+  botao:{
+    width:230,
+    height:50,
+    borderWidth:2,
+    borderColor: '#dd7b22',
+    borderRadius: 25 
+  },
+  area:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  texto:{
+    color: '#dd7b22',
+    fontSize:20,
+    fontWeight: 'bold'
+  },
+  textoFrase:{
+    fontSize:20,
+    color: '#dd7b22',
+    margin:30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  }
+
 });
